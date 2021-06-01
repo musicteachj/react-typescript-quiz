8754d5896b436b72f9452a4918c00f17860e1608
James Littlefield <jimilittle3@hotmail.com>
2021-06-01 11:59:31 -0400

    setup check anser and next question methods

--- src/App.tsx
+++ src/App.tsx
@@ -41,9 +41,36 @@ const App = () => {
   };
 
   // take in event
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      // User answer
+      const answer = e.currentTarget.value;
+      //check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // Add score if answer is correct
+      if (correct) setScore(prev => prev + 1);
+      // Save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      }
+      setuserAnswers((prev) => [...prev, answerObject])
+    }
+  };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    // Move on to the next question if not the last question
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true)
+    } else {
+      setNumber(nextQuestion)
+    }
+
+  };
 
   return (
     <div className="App">
